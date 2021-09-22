ff1e1a4eec0b02a2801d9172c02a3c9ab29a0b9c
shubhi2705 <shubhisinghal2705@gmail.com>
2021-09-22 23:26:41 +0530

    fifth commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,5 @@
 // Import the functions you need from the SDKs you need
 import * as firebase from 'firebase'
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDKqZz0vRFOiFIS1qNoPaBhBvje_xAQfmA",
   authDomain: "ninja-firegram-1a84f.firebaseapp.com",
