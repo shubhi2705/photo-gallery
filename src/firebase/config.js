f9f89a84b7dd4cf1cc64de82e8d813c02f9dac18
shubhi2705 <shubhisinghal2705@gmail.com>
2021-09-22 23:09:18 +0530

    second commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import * as firebase from 'firebase'
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,13 +14,12 @@ const firebaseConfig = {
   appId: "1:430091454439:web:d6e9ff068e496afc2a1f42"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 export {projectFirestore,projectStorage,timestamp};
