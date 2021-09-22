8b1ad270ef4dafb44a453b7067fa3056b39f6cde
shubhi2705 <shubhisinghal2705@gmail.com>
2021-09-22 22:58:56 +0530

    initial commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKqZz0vRFOiFIS1qNoPaBhBvje_xAQfmA",
+  authDomain: "ninja-firegram-1a84f.firebaseapp.com",
+  projectId: "ninja-firegram-1a84f",
+  storageBucket: "ninja-firegram-1a84f.appspot.com",
+  messagingSenderId: "430091454439",
+  appId: "1:430091454439:web:d6e9ff068e496afc2a1f42"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
